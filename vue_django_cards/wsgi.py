61ac4a21ceb0c4082cc3e85c96de23290f326151
Igor <devNoon@yandex.ru>
2023-10-29 23:08:51 +0300

    changes for vercel deploy

--- vue_django_cards/wsgi.py
+++ vue_django_cards/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vue_django_cards.settings')
 
 application = get_wsgi_application()
+app = application
